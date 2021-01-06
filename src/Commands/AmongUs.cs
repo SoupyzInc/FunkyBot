5d9efc4aadad4250d84a9ae6634f9f4ef8be8da9
Matthew Okashita <okashita.matthew@gmail.com>
2021-01-06 07:06:49 -0800

    Added xaml comments

--- src/Commands/AmongUs.cs
+++ src/Commands/AmongUs.cs
@@ -1,15 +1,108 @@
-﻿//System
+using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Linq;
-
-//Discord.Net
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 
+/// <summary>
+/// Contains all necessary methods to moderate Among Us games.
+/// </summary>
 public class AmongUsMoudle : ModuleBase<SocketCommandContext>
 {
+    /// <summary>
+    /// Checks for a message calling the unmute operation
+    /// </summary>
+    /// <param name="deadPersonsGiven">dead players who should remain muted.</param>
+    /// <remarks>Umutes all players in the voice channel who are not pinged in the command.</remarks>
+    [Command("unmute", RunMode = RunMode.Async)] //Allows for the method to run on a thread different from the gateway
+    public async Task Unmute(params IUser[] deadPersonsGiven)
+    {
+        IVoiceChannel userChannel = (Context.User as IVoiceState).VoiceChannel; //Gives reference to the users voice channel
+
+        //User Not In VC Error Handler
+        try
+        {
+            var voiceUsers = Context.Guild.GetVoiceChannel(userChannel.Id).Users; //Attempts to grab all people in the vc
+        }
+        catch
+        {
+            //Error Handler Message
+            var builderTest = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder
+                {
+                    Name = "Join a voice channel to use the Unmute command.",
+                },
+
+                Footer = new EmbedFooterBuilder
+                {
+                    Text = "Requested by " + Context.User,
+                    IconUrl = Context.User.GetAvatarUrl()
+                },
+            };
+
+            builderTest.WithColor(0xFEFEFE)
+                    .Build();
+
+            await Context.Channel.SendMessageAsync(embed: builderTest.Build());
+
+            return;
+        }
+        finally
+        {
+            await ReplyAsync(embed: ManageMute(Context, false, deadPersonsGiven));
+        }
+    }
+
+    /// <summary>
+    /// Checks for a messag calling the mute operation.
+    /// </summary>
+    /// <remarks>Mutes all players in the voice channel.</remarks>
+    [Command("mute", RunMode = RunMode.Async)] //Allows for the method to run on a thread different from the gateway
+    public async Task NewMute()
+    {
+        IVoiceChannel userChannel = (Context.User as IVoiceState).VoiceChannel; //Gives reference to the users voice channel
+
+        try //Checks if user is in a voice channel
+        {
+            var voiceUsers = Context.Guild.GetVoiceChannel(userChannel.Id).Users; //Attempts to grab all people in the vc
+        }
+        catch //If not, returns an error message
+        {
+            //Error Handler Message
+            var builderTest = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder
+                {
+                    Name = "Join a voice channel to use the mute command.",
+                },
+
+                Footer = new EmbedFooterBuilder
+                {
+                    Text = "Requested by " + Context.User,
+                    IconUrl = Context.User.GetAvatarUrl()
+                },
+            };
+
+            builderTest.WithColor(0xFEFEFE)
+                    .Build();
+
+            await Context.Channel.SendMessageAsync(embed: builderTest.Build());
+        }
+        finally //If so, 
+        {
+            await ReplyAsync(embed: ManageMute(Context, true));
+        }
+    }
+
+    /// <summary>
+    /// Determines who in the voice channel should be muted or not and performs the mute ooperation.
+    /// </summary>
+    /// <param name="Context">the context of the users in the voice channel.</param>
+    /// <param name="doMute">whether to mute or unmute the voice channel.</param>
+    /// <param name="deadPeopleGiven">dead users who should remain muted and exluced from the mute operation.</param>
+    /// <returns>And embeded message to be printed confirming the mute operation.</returns>
     public Embed ManageMute(SocketCommandContext Context, bool doMute, params IUser[] deadPeopleGiven)
     {
         IVoiceChannel userChannel = (Context.User as IVoiceState).VoiceChannel; //Gives reference to the users voice channel
@@ -77,6 +170,12 @@ public class AmongUsMoudle : ModuleBase<SocketCommandContext>
         }
     }
 
+    /// <summary>
+    /// Mutes and unmutes necessary players in the voice channel.
+    /// </summary>
+    /// <param name="Operation">whether to mute or unmute necessary players</param>
+    /// <param name="UsersToMute">a list of users to mute.</param>
+    /// <param name="deadPeople">a list of dead players who should remain muted.</param>
     public async void MuteOperation(bool Operation, List<SocketGuildUser> UsersToMute, List<SocketGuildUser> deadPeople)
     {
         if (Operation == true) //Mute
@@ -93,120 +192,6 @@ public class AmongUsMoudle : ModuleBase<SocketCommandContext>
             {
                 await user.ModifyAsync(m => { m.Mute = false; });
             }
-        }
-    }
-
-    [Command("unmute", RunMode = RunMode.Async)] //Allows for the method to run on a thread different from the gateway
-    public async Task Unmute(params IUser[] deadPersonsGiven)
-    {
-        IVoiceChannel userChannel = (Context.User as IVoiceState).VoiceChannel; //Gives reference to the users voice channel
-
-        //User Not In VC Error Handler
-        try
-        {
-            var voiceUsers = Context.Guild.GetVoiceChannel(userChannel.Id).Users; //Attempts to grab all people in the vc
-        }
-        catch
-        {
-            //Error Handler Message
-            var builderTest = new EmbedBuilder
-            {
-                Author = new EmbedAuthorBuilder
-                {
-                    Name = "Join a voice channel to use the Unmute command.",
-                },
-
-                Footer = new EmbedFooterBuilder
-                {
-                    Text = "Requested by " + Context.User,
-                    IconUrl = Context.User.GetAvatarUrl()
-                },
-            };
-
-            builderTest.WithColor(0xFEFEFE)
-                    .Build();
-
-            await Context.Channel.SendMessageAsync(embed: builderTest.Build());
-
-            return;
-        }
-        finally
-        {
-            await ReplyAsync(embed: ManageMute(Context, false, deadPersonsGiven));
-
-            /*
-            var builder = new EmbedBuilder
-            {
-                Description = ":speaker: **User(s) have been unmuted.**",
-
-                Footer = new EmbedFooterBuilder
-                {
-                    Text = "Requested by " + Context.User,
-                    IconUrl = Context.User.GetAvatarUrl()
-                },
-            };
-
-            builder.WithColor(0xFEFEFE)
-                    .Build();
-
-            await ReplyAsync(embed: builder.Build());
-            */
-        }
-    }
-
-    [Command("mute", RunMode = RunMode.Async)] //Allows for the method to run on a thread different from the gateway
-    public async Task NewMute()
-    {
-        IVoiceChannel userChannel = (Context.User as IVoiceState).VoiceChannel; //Gives reference to the users voice channel
-
-        //User Not In VC Error Handler
-        try
-        {
-            var voiceUsers = Context.Guild.GetVoiceChannel(userChannel.Id).Users; //Attempts to grab all people in the vc
-        }
-        catch
-        {
-            //Error Handler Message
-            var builderTest = new EmbedBuilder
-            {
-                Author = new EmbedAuthorBuilder
-                {
-                    Name = "Join a voice channel to use the mute command.",
-                },
-
-                Footer = new EmbedFooterBuilder
-                {
-                    Text = "Requested by " + Context.User,
-                    IconUrl = Context.User.GetAvatarUrl()
-                },
-            };
-
-            builderTest.WithColor(0xFEFEFE)
-                    .Build();
-
-            await Context.Channel.SendMessageAsync(embed: builderTest.Build());
-        }
-        finally
-        {
-            await ReplyAsync(embed: ManageMute(Context, true));
-
-            /*
-            var builder = new EmbedBuilder
-            {
-                Description = ":mute: **User(s) have been muted.**",
-
-                Footer = new EmbedFooterBuilder
-                {
-                    Text = "Requested by " + Context.User,
-                    IconUrl = Context.User.GetAvatarUrl()
-                },
-            };
-
-            builder.WithColor(0xFEFEFE)
-                    .Build();
-
-            await ReplyAsync(embed: builder.Build());
-            */
         }
     }
 }
